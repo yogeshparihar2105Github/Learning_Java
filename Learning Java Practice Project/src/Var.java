30b286df1a7361caa2b18d880eb3306c525276b8
Yogesh Parihar <yogeshparihar2105@gmail.com>
2022-07-05 14:38:39 +0530

    vari

---
+++ Learning Java Practice Project/src/Var.java
@@ -0,0 +1,16 @@
+public class Var {
+    public static void main(String[] args) {
+        
+        int a = 2;
+        float b = 2.0f;
+        char c = 'c';
+        String name = "My name is Giovani Georgio";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(name);
+        
+        
+    }
+}
